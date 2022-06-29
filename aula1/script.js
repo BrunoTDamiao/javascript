f3a75877ccef10104c8f3b47fff0ce3741c1cdfc
Bruno Damiao <104789324+BrunoTDamiao@users.noreply.github.com>
2022-06-29 19:22:26 -0300

    comecei a estudar javascript

---
+++ aula1/script.js
@@ -0,0 +1,10 @@
+var nome = 'Bruno', idade = 22, comida ='';
+console.log(nome, idade, comida);
+
+comida = 'lasanha';
+
+console.log(nome, idade, comida);
+
+var var1, var2, var3, var4, var5
+
+console.log(var1, var2, var3, var4, var5);
